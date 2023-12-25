8e53dc5d067bd25578ea4f125cab61f9ffdac0ba
Geizka Rozilia Ruicosta <70560582+Shercosta@users.noreply.github.com>
2023-12-25 22:07:42 +0700

    Initial commit

---
+++ yourappname/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("other-route/", views.other_route, name="other_route_name"),
+]
